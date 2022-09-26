f50415c9fbc9dd1eca7802496cff6ea5646dccdd
uravilla <ushmitharavilla998@gmail.com>
2022-09-26 19:05:46 -0400

    updates

--- content-data.js
+++ content-data.js
@@ -92,7 +92,7 @@ const Experiences = {
             text1: 'Worked on PL/SQL to update and view the required data. Used Angular Delphi to create application UI.',
             text2: 'Mentored four interns where I had to communicate effectively, review code changes and provide constructive feedback in a way that fostered growth and development.',
             text3: 'Developed features based on client requirements using Spring MVC, Java, Delphi, Oracle technologies to enhance our core product (Rating and Billing Manager) and performed unit testing for the implemented features. Involved in all phases of SDLC.',
-            text4: 'Outcome: Gained experience using PLSQL, UI, and Frameworks and understanding the software development lifecycle. Mentoring experience to other interns.'
+            text4: 'Outcome: Gained experience in using PLSQL, UI, and Frameworks and understanding the software development lifecycle. Mentoring experience to other interns.'
         }
     },
     2: {
@@ -113,7 +113,7 @@ const Experiences = {
         role: 'Project Mentee',
         texts: {
             text1: 'Built a web application for simplifying peer-to-peer selling for new or previously owned products like Fashion, mobile, books, and much more using Spring MVC, Hibernate frameworks. Holding additional Functionalities like in-app messaging, tracking the order, delivery, payments, and option to follow each other Users.',
-            text2: 'Outcome: Gained experience using Frameworks such as Spring, Hibernate',
+            text2: 'Outcome: Gained experience in using Frameworks such as Spring, Hibernate',
             text3: '',
             text4: ''
         }
@@ -137,7 +137,7 @@ const Projects = {
         tool: 'Tools Used - Salesforce Platform Apex, Visual Force',
         texts: {
             text1: 'A web Seminar Hall Management System is a Salesforce Application that helps users to either host or buy the tickets for an event at a chosen Seminar Hall whose location is also shown on Google maps. Here, the Admin, who can manage the users of this application monitor all the operations.',
-            text2: 'Outcome: Hands experience in using the salesforce platform, building apps, and deploying them.'
+            text2: 'Outcome: Hands-on experience in using the salesforce platform, building apps, and deploying them.'
         }
     },
     2: {
@@ -146,7 +146,7 @@ const Projects = {
         tool: 'Tools Used - Salesforce Platform',
         texts: {
             text1: 'An app that makes it easy to manage & Predict the peak cherry blossom date for three different locations (Washington DC, Kyoto, Liestal) based on the location latitude, longitude, and altitude using existing data. Used regression models to predict the number of days it will take for the cherry to blossom every year. ales and enables you to invoice clients, Fast receive and track payments online and view the history of your transactions. Manage recurring Invoices and payments in business with the optimal method of communicating with your clients.',
-            text2: 'Outcome: Hands experience in using the salesforce platform, building apps, and deploying them.'
+            text2: 'Outcome: Hands-on experience in using the salesforce platform, building apps, and deploying them.'
         }
     }
 }
